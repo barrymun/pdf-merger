628ecc5d89b080592f6bc4165123cbf3adab290d
Neil Murphy <barrymun@tcd.ie>
2023-10-29 18:12:31 +0000

    working merge pdf functionality (using pdf-lib package)

--- src/components/uploader.ts
+++ src/components/uploader.ts
@@ -1,3 +1,4 @@
+import { PDFDocument } from "pdf-lib";
 import van from "vanjs-core";
 
 import { uploaderInputId } from "utils/constants";
@@ -5,8 +6,43 @@ import { uploaderInputId } from "utils/constants";
 const { button, div, input } = van.tags;
 
 export const Uploader = () => {
-  const handleClick = (event: Event) => {
-    console.log(event);
+  const mergePDFs = async () => {
+    const pdfDocs = [];
+    const pdfInput = document.getElementById(uploaderInputId)! as HTMLInputElement;
+    const files = pdfInput.files;
+    console.log(files);
+
+    if (!files) {
+      return;
+    }
+
+    // Load the PDFs into PDFDocument instances
+    for (let i = 0; i < files.length; i++) {
+      const file = files[i];
+      const arrayBuffer = await file.arrayBuffer();
+      const pdfDoc = await PDFDocument.load(arrayBuffer);
+      pdfDocs.push(pdfDoc);
+    }
+
+    // Create a new PDFDocument
+    const mergedPdf = await PDFDocument.create();
+
+    // Copy pages from the loaded PDFs into the new PDF
+    for (const pdfDoc of pdfDocs) {
+      const pageCount = pdfDoc.getPageCount();
+      for (let i = 0; i < pageCount; i++) {
+        const [page] = await mergedPdf.copyPages(pdfDoc, [i]);
+        mergedPdf.addPage(page);
+      }
+    }
+
+    // Serialize the merged PDF to bytes and provide it for download
+    const mergedPdfBytes = await mergedPdf.save();
+    const blob = new Blob([mergedPdfBytes], { type: "application/pdf" });
+    const link = document.createElement("a");
+    link.href = URL.createObjectURL(blob);
+    link.download = "merged.pdf";
+    link.click();
   };
 
   return div(
@@ -20,7 +56,7 @@ export const Uploader = () => {
     }),
     button(
       {
-        onclick: handleClick,
+        onclick: mergePDFs,
       },
       "Merge PDFs",
     ),
